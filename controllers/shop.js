33c5aeb623ddbbcfc3f7cdda338c05e7469ed7f1
chukwutem-emi <chukwutememi@gmail.com>
2026-01-29 19:45:08 +0100

    updated

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,17 +4,31 @@ const fs = require("fs");
 const path = require("path");
 const PDFDocument = require("pdfkit")
 
+const ITEMS_PER_PAGE = 1
 
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
-    .then(products => {
-      console.log(products);
-      res.render('shop/product-list', {
-        prods: products,
-        pageTitle: 'All Products',
-        path: '/products',
-      });
-    })
+  .countDocuments()
+  .then((numProducts) => {
+    totalItems = numProducts;
+    return Product.find().skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE)
+  })
+  .then(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'Products',
+      path: '/products',
+      currentPage: page,
+      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+      hasPreviousPage: page > 1,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+    });
+  })
     .catch(err => {
       console.log(err);
     });
@@ -34,19 +48,33 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
-    .then(products => {
-      res.render('shop/index', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/'
-      });
-    })
-    .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+  .countDocuments()
+  .then((numProducts) => {
+    totalItems = numProducts;
+    return Product.find().skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE)
+  })
+  .then(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      currentPage: page,
+      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+      hasPreviousPage: page > 1,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
     });
+  })
+  .catch(err => {
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(error);
+  });
 };
 
 exports.getCart = (req, res, next) => {
@@ -84,6 +112,25 @@ exports.postCartDeleteProduct = (req, res, next) => {
     })
     .catch(err => console.log(err));
 };
+
+exports.getCheckout = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    .then(user => {
+      const products = user.cart.items;
+      let total = 0;
+      products.forEach((p) => {
+        total += p.quantity * p.productId.price
+      })
+      res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout',
+        products: products,
+        totalSum: total
+      });
+    })
+    .catch(err => console.log(err));
+}
 
 exports.postOrder = (req, res, next) => {
   req.user
---
+++ public/js/admin.js
@@ -0,0 +1,22 @@
+console.log("Admin.js loaded")
+const deleteProduct = (btn) => {
+    const prodId = btn.parentNode.querySelector("[name=productId]").value;
+
+    const productElement = btn.closest("article");
+
+    fetch("/admin/product/" + prodId, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then((result) => {
+        return result.json();
+    })
+    .then((data) => {
+        console.log(data);
+        productElement.parentNode.removeChild(productElement);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+};
